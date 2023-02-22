8ec6b097a65f8ec1479cfaf87b1d6c1a0ee35f0f
Ramit Goolry <ramitgoolry@gmail.com>
2023-02-22 05:40:08 +0530

    Made it work with vim

--- lua/rapid_return/init.lua
+++ lua/rapid_return/init.lua
@@ -1,6 +1,4 @@
-local stack = require("stack")
-
-local M = {}
+local stack = require("rapid_return.stack")
 
 function push_cursor()
   stack.push({vim.fn.line("."), vim.fn.col(".")})
@@ -8,7 +6,13 @@ end
 
 function pop_cursor()
   local pos = stack.pop()
+
   if pos then
     vim.fn.cursor(pos[1], pos[2])
   end
 end
+
+return {
+  push_cursor = push_cursor,
+  pop_cursor = pop_cursor
+}
