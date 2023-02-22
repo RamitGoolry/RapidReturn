4d6626a9d9c32d30d81802e4f252b10d7b6d612b
Ramit Goolry <ramitgoolry@gmail.com>
2023-02-22 08:37:10 +0530

    Added little indicators for where the pointers are being placed

--- lua/rapid_return/init.lua
+++ lua/rapid_return/init.lua
@@ -2,6 +2,8 @@ local stack = require("rapid_return.stack")
 
 local M = {}
 
+local text = "⚓" -- text to display
+
 function M.push_cursor()
   local top = stack.top()
 
@@ -13,7 +15,9 @@ function M.push_cursor()
   end
 
   stack.push({vim.fn.line('.'), vim.fn.col('.'), vim.fn.expand('%')})
-  print("Cursor position stored")
+
+  -- Set cursor locations
+  vim.api.nvim_buf_set_virtual_text(0, 0, vim.fn.line('.') - 1, {{text .. stack.size(), "LineNr"}}, {})
 end
 
 function M.pop_cursor()
@@ -22,6 +26,10 @@ function M.pop_cursor()
   if pos then
     vim.cmd('edit ' .. pos[3])
     vim.fn.cursor(pos[1], pos[2])
+
+    -- Remove cursor locations
+    vim.api.nvim_buf_clear_namespace(0, 0, pos[1] - 1, pos[1])
+    vim.api.nvim_buf_set_virtual_text(0, 0, pos[1] - 1, {}, {})
   else
     vim.api.nvim_err_write("No more cursors to pop")
     print("No more cursors to pop")
@@ -30,6 +38,7 @@ end
 
 function M.clear()
   stack.clear()
+  -- TODO Clear all signs
   print("Cleared all cursors")
 end
 
